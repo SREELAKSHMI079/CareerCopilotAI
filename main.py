99cce489ae292c29b0e6f4b31107cffac3bdbae2
Sreelakshmi A <sreelakshmia2005@gmail.com>
2026-08-18 20:16:56 +0530

    Implement JWT authentication with protected routes

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from fastapi import FastAPI
-from database import engine, Base,SessionLocal
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from database import engine, Base, SessionLocal
 import models
 from schemas import UserCreate
 from schemas import UserLogin
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from models import User 
-from auth import hash_password
-from auth import verify_password
-
-from fastapi import HTTPException
-
+from auth import (hash_password,verify_password,create_access_token,verify_access_token)
 
 Base.metadata.create_all(bind=engine)
 app=FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 @app.post("/register")
@@ -31,9 +30,9 @@ def register_user(user: UserCreate):
         "message":"REGISTRATION SUCCESSFULL"
     }
 @app.post("/login")
-def login_user(user:UserLogin):
+def login_user(user: OAuth2PasswordRequestForm = Depends()):
     db=SessionLocal()
-    existing_user=db.query(User).filter(User.email==user.email).first()
+    existing_user = db.query(User).filter(User.email == user.username).first()
     if existing_user is None:
         raise HTTPException(
             status_code=401,
@@ -45,7 +44,29 @@ def login_user(user:UserLogin):
     ):
         raise HTTPException(status_code=401,detail="Invalid email or password"
         )
+    access_token=create_access_token(data={"sub": existing_user.email})
     return {
-        "message":"LOGIN SUCCESSFULL-WELCOME TO CAREERCOPILOT"
+        "access_token":access_token,
+        "token_type":"bearer"
     }
-    
+@app.get("/me")
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    email = verify_access_token(token)
+
+    db = SessionLocal()
+
+    existing_user = db.query(User).filter(
+        User.email == email
+    ).first()
+
+
+    if existing_user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+
+    return {
+        "full_name": existing_user.full_name,
+        "email": existing_user.email
+    }
